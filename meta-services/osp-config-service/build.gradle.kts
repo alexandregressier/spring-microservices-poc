df1610c06c147f05ca5c4b9c8cbdf1cdbb57da65
Alexandre Gressier <alexandre@gressier.dev>
2021-01-13 00:15:45 +0100

    Make the Config Service serve configurations

--- meta-services/osp-config-service/build.gradle.kts
+++ meta-services/osp-config-service/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     // Kotlin
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     // Ops
     implementation("org.springframework.boot:spring-boot-starter-actuator")
--- meta-services/osp-config-service/src/main/kotlin/dev/gressier/osp/meta/services/config/App.kt
+++ meta-services/osp-config-service/src/main/kotlin/dev/gressier/osp/meta/services/config/App.kt
@@ -2,8 +2,10 @@ package dev.gressier.osp.meta.services.config
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
+@EnableConfigServer
 class App
 
 fun main(args: Array<String>) {
