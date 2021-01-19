59fe385e17815bf416ef4733c2646fae18f22679
Alexandre Gressier <alexandre@gressier.dev>
2021-01-19 21:56:27 +0100

    Reorder the annotations of the bootstrap classes

--- meta-services/osp-config-service/src/main/kotlin/dev/gressier/osp/meta/services/config/App.kt
+++ meta-services/osp-config-service/src/main/kotlin/dev/gressier/osp/meta/services/config/App.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.config.server.EnableConfigServer
 
-@SpringBootApplication
 @EnableConfigServer
+@SpringBootApplication
 class App
 
 fun main(args: Array<String>) {
