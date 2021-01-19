8bc0cc73323135af88a0367310520ac181e24ed3
Alexandre Gressier <alexandre@gressier.dev>
2021-01-19 22:43:18 +0100

    Enable the Eureka server in the Eureka Service

--- meta-services/osp-eureka-service/src/main/kotlin/dev/gressier/osp/meta/services/eureka/App.kt
+++ meta-services/osp-eureka-service/src/main/kotlin/dev/gressier/osp/meta/services/eureka/App.kt
@@ -2,7 +2,9 @@ package dev.gressier.osp.meta.services.eureka
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
+@EnableEurekaServer
 @SpringBootApplication
 class App
 
