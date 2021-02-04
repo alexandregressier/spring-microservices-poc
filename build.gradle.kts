0a7a93367b5bbf51c524b08e5f1c8664375df8a2
Alexandre Gressier <alexandre@gressier.dev>
2021-02-04 15:18:10 +0100

    Justify the content of root Gradle build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     // Kotlin
     val kotlinVersion = "1.4.21"
-    kotlin("jvm") version kotlinVersion apply false
-    kotlin("kapt") version kotlinVersion apply false
+    kotlin("jvm")           version kotlinVersion apply false
+    kotlin("kapt")          version kotlinVersion apply false
     kotlin("plugin.spring") version kotlinVersion apply false
-    kotlin("plugin.jpa") version kotlinVersion apply false
+    kotlin("plugin.jpa")    version kotlinVersion apply false
 
     // Spring
-    id("org.springframework.boot") version "2.4.3-SNAPSHOT" apply false
+    id("org.springframework.boot")        version "2.4.3-SNAPSHOT" apply false
     id("io.spring.dependency-management") version "1.0.10.RELEASE" apply false
 }
