81a3e57c1fd6aa35f28e113c6c46b8aaea9db8ba
Alexandre Gressier <alexandre@gressier.dev>
2021-02-10 22:45:43 +0100

    Reorder the annotations on the bootstrap class of the License service

--- services/osp-license-service/src/main/kotlin/dev/gressier/osp/services/license/App.kt
+++ services/osp-license-service/src/main/kotlin/dev/gressier/osp/services/license/App.kt
@@ -9,9 +9,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.Bean
 import org.springframework.web.client.RestTemplate
 
-@ConfigurationPropertiesScan("dev.gressier.osp.services.license.config")
 @EnableFeignClients
 @EnableDiscoveryClient
+@ConfigurationPropertiesScan("dev.gressier.osp.services.license.config")
 @SpringBootApplication
 class App {
 
