9e75f420d892d1a1a294858c53b85f2d375dc169
Alexandre Gressier <alexandre@gressier.dev>
2021-02-11 20:58:00 +0100

    Implement the retry pattern for Organization querying

--- services/osp-license-service/src/main/kotlin/dev/gressier/osp/services/license/controller/client/OrganizationRestClient.kt
+++ services/osp-license-service/src/main/kotlin/dev/gressier/osp/services/license/controller/client/OrganizationRestClient.kt
@@ -2,21 +2,28 @@ package dev.gressier.osp.services.license.controller.client
 
 import dev.gressier.osp.services.license.model.Organization
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
-import mu.KotlinLogging
+import io.github.resilience4j.retry.annotation.Retry
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 import java.util.*
-
-private val log = KotlinLogging.logger {}
 
 @Component
 class OrganizationRestClient {
 
     @Autowired private lateinit var restTemplate: RestTemplate
 
+    @Retry(name = "#root.methodName")
     @CircuitBreaker(name = "#root.methodName")
-    fun getOrganization(id: UUID): Organization? {
-        return restTemplate.getForObject("http://organization-service/organizations/{id}", Organization::class.java, id)
-    }
+    fun getOrganization(id: UUID): Organization? =
+        restTemplate.getForObject("http://organization-service/organizations/{id}", Organization::class.java, id)
+
+    fun buildBlankOrganization(id: UUID, t: Throwable): Organization? =
+        Organization(
+            id = UUID(0, 0),
+            name = "(Unavailable)",
+            contactName = "No organization information is available at the moment",
+            contactEmail = "",
+            contactPhone = "",
+        )
 }
