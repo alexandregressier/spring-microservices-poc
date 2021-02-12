e5a05a6568851b372ef18bcd0f427b196996784e
Alexandre Gressier <alexandre@gressier.dev>
2021-02-12 14:07:58 +0100

    Add loggers to Resilience4j circuit breaker & retry instances

--- services/osp-license-service/src/main/kotlin/dev/gressier/osp/services/license/App.kt
+++ services/osp-license-service/src/main/kotlin/dev/gressier/osp/services/license/App.kt
@@ -1,6 +1,13 @@
 package dev.gressier.osp.services.license
 
 import dev.gressier.osp.services.license.config.Config
+import io.github.resilience4j.circuitbreaker.CircuitBreaker
+import io.github.resilience4j.core.registry.EntryAddedEvent
+import io.github.resilience4j.core.registry.EntryRemovedEvent
+import io.github.resilience4j.core.registry.EntryReplacedEvent
+import io.github.resilience4j.core.registry.RegistryEventConsumer
+import io.github.resilience4j.retry.Retry
+import mu.KotlinLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
@@ -8,7 +15,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Profile
 import org.springframework.web.client.RestTemplate
+
+private val log = KotlinLogging.logger {}
 
 @EnableFeignClients
 @EnableDiscoveryClient
@@ -16,9 +26,35 @@ import org.springframework.web.client.RestTemplate
 @SpringBootApplication
 class App {
 
-    @Bean
+    // RestTemplate with Eureka client
+
     @LoadBalanced
+    @Bean
     fun restTemplate() = RestTemplate()
+
+    // Resilience4j
+
+    @Profile("dev")
+    @Bean
+    fun customCircuitBreakerRegistryEventConsumer(): RegistryEventConsumer<CircuitBreaker> =
+        object : RegistryEventConsumer<CircuitBreaker> {
+            override fun onEntryAddedEvent(entryAddedEvent: EntryAddedEvent<CircuitBreaker>) =
+                entryAddedEvent.addedEntry.eventPublisher.onEvent { log.info("$it") }
+
+            override fun onEntryRemovedEvent(entryRemoveEvent: EntryRemovedEvent<CircuitBreaker>) {}
+            override fun onEntryReplacedEvent(entryReplacedEvent: EntryReplacedEvent<CircuitBreaker>) {}
+        }
+
+    @Profile("dev")
+    @Bean
+    fun customRetryRegistryEventConsumer(): RegistryEventConsumer<Retry> =
+        object : RegistryEventConsumer<Retry> {
+            override fun onEntryAddedEvent(entryAddedEvent: EntryAddedEvent<Retry>) =
+                entryAddedEvent.addedEntry.eventPublisher.onEvent { log.info("$it") }
+
+            override fun onEntryRemovedEvent(entryRemoveEvent: EntryRemovedEvent<Retry>) {}
+            override fun onEntryReplacedEvent(entryReplacedEvent: EntryReplacedEvent<Retry>) {}
+        }
 }
 
 fun main(args: Array<String>) {
