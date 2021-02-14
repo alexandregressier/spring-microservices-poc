2aa282c3fe3b33d64298be81b96b0561eec21852
Alexandre Gressier <alexandre@gressier.dev>
2021-02-14 22:36:45 +0100

    Implement the user context in the Organization service

--- services/osp-organization-service/src/main/kotlin/dev/gressier/osp/services/organization/App.kt
+++ services/osp-organization-service/src/main/kotlin/dev/gressier/osp/services/organization/App.kt
@@ -1,15 +1,21 @@
 package dev.gressier.osp.services.organization
 
+import dev.gressier.osp.commons.context.UserContextFilter
 import dev.gressier.osp.services.organization.config.Config
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.context.annotation.Bean
 
 @EnableDiscoveryClient
 @EnableConfigurationProperties(Config::class)
 @SpringBootApplication
-class App
+class App {
+
+    @Bean
+    fun userContextFilter() = UserContextFilter()
+}
 
 fun main(args: Array<String>) {
     runApplication<App>(*args)
