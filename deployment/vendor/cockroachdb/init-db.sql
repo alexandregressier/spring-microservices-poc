177fa38074c073b809d734016e9a36c6a6db271c
Alexandre Gressier <alexandre@gressier.dev>
2021-02-17 20:39:13 +0100

    Remove the `_user` suffix for database users

--- deployment/vendor/cockroachdb/init-db.sql
+++ deployment/vendor/cockroachdb/init-db.sql
@@ -4,10 +4,10 @@ DROP DATABASE postgres;
 
 -- OSP Organization Service
 CREATE DATABASE osp_organization_service;
-CREATE USER osp_organization_service_user;
-GRANT ALL ON DATABASE osp_organization_service TO osp_organization_service_user;
+CREATE USER osp_organization_service;
+GRANT ALL ON DATABASE osp_organization_service TO osp_organization_service;
 
 -- OSP License Service
 CREATE DATABASE osp_license_service;
-CREATE USER osp_license_service_user;
-GRANT ALL ON DATABASE osp_license_service TO osp_license_service_user;
+CREATE USER osp_license_service;
+GRANT ALL ON DATABASE osp_license_service TO osp_license_service;
