31d70ff124ec53bdd36de2a899e7f95a219a1010
Alexandre Gressier <alexandre@gressier.dev>
2021-03-03 15:06:49 +0100

    Define variables for the version of each Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,16 @@
 plugins {
+    // Versions
+    val versionKotlin                     = "1.4.21"
+    val versionSpringBoot                 = "2.4.4-SNAPSHOT"
+    val versionSpringDependencyManagement = "1.0.11.RELEASE"
+
     // Kotlin
-    val kotlinVersion = "1.4.21"
-    kotlin("jvm")           version kotlinVersion apply false
-    kotlin("kapt")          version kotlinVersion apply false
-    kotlin("plugin.spring") version kotlinVersion apply false
-    kotlin("plugin.jpa")    version kotlinVersion apply false
+    kotlin("jvm")                         version versionKotlin apply false
+    kotlin("kapt")                        version versionKotlin apply false
+    kotlin("plugin.spring")               version versionKotlin apply false
+    kotlin("plugin.jpa")                  version versionKotlin apply false
 
     // Spring
-    id("org.springframework.boot")        version "2.4.4-SNAPSHOT" apply false
-    id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
+    id("org.springframework.boot")        version versionSpringBoot apply false
+    id("io.spring.dependency-management") version versionSpringDependencyManagement apply false
 }
